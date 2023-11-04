85f0f2a5777379b389adf9d97728b00e100c2418
FZ688 <2307839919@qq.com>
2023-11-04 23:37:31 +0800

    第二次提交

---
+++ DdlSystem/src/Main.java
@@ -0,0 +1,10 @@
+import com.tudg.basic.DDL;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        DDL aviodddl =new DDL();
+        aviodddl.startMenu();
+        }
+}
